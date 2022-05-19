83a89238e1a73346875dc92ed2c5ef8a94d844dc
samkenxstream <100836531+samkenxstream@users.noreply.github.com>
2022-05-19 10:29:38 +0800

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/myapplicationcootek/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplicationcootek/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplicationcootek;
+package com.example.applicationshieldproject;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.myapplicationcootek", appContext.getPackageName());
+        assertEquals("com.example.applicationshieldproject", appContext.getPackageName());
     }
 }
